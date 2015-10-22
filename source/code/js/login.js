74e2b37ce4c7989c8a5fdd50d347a56cb4b24439
appue <appue@github.com>
2015-10-22 22:08:05 +0800

    modify

--- source/code/js/login.js
+++ source/code/js/login.js
@@ -1,8 +1,32 @@
 angular.module('wxSDK')
 .controller('tLogin', function (
     $scope,
-    $http,
-    $state
+    $state,
+    widget
 ){
     $scope.currentName = "账号绑定";
+
+    $scope.tInput = {};
+
+    $scope.setLogin = function () {
+        if (!$scope.tInput.UserName) {
+            widget.msgToast('请输入用户名');
+            return;
+        }
+        if (!$scope.tInput.Password) {
+            widget.msgToast('请输入密码');
+            return;
+        }
+
+        widget.ajaxRequest({
+            scope: $scope,
+            url: '',
+            data: {
+
+            },
+            success: function (res) {
+                
+            }
+        });
+    };
 });
